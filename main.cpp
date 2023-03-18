46a9731c180f47fe5592992998d638210ca95b54
ns3409999 <121076165+ns3409999@users.noreply.github.com>
2023-03-17 19:13:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
-#include "q2.hpp"
-#include <iomanip>
+#include <iostream>
 using namespace std;
 
-//********************
-int main()
-{
-	int n1, n2, n3;
-	int min;
-	getInput(n1, n2, n3);
-	min = findMin(n1, n2, n3);
-	printResult(n1, n2, n3, min);
+int main() {
+    
+    double n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+
+    // check if n1 is the largest number
+    if(n1 >= n2 && n1 >= n3)
+        cout << "Largest number: " << n1;
+
+    // check if n2 is the largest number
+    else if(n2 >= n1 && n2 >= n3)
+        cout << "Largest number: " << n2;
+    
+    // if neither n1 nor n2 are the largest, n3 is the largest
+    else 
+        cout << "Largest number: " << n3;
+  
+    return 0;
 }
